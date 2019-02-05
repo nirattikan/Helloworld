a174048ceb81882fafbca126a46e5fb1accd7562
NIRATTIKAN LAEWARIT <5831302008@lamduan.mfu.ac.th>
2019-02-05 13:42:20 +0700

    next js

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+
+import Link from 'next/link'
+
+const Index = () => (
+  <div>
+    <Link href="/">
+      <a>Index Page</a>
+    </Link>
+    <h1>About</h1>
+  </div>
+)
+
+export default Index
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,13 @@
-import Link from 'next'
+import Link from 'next/link'
+
+const Index = () => (
+  <div>
+    <Link href="/about">
+      <a>About Page</a>
+    </Link>
+    <h1>Sawatdee Next.js</h1>
+    <p>Index</p>
+  </div>
+)
+
+export default Index
